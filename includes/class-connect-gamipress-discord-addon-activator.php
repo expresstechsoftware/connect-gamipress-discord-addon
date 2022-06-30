c0cc70fb15808f351e1d167d16305f4b38a835b3
younes-dro <dro66_8@hotmail.fr>
2022-06-30 09:45:15 +0100

    Set default options on activation the plugin

--- includes/class-connect-gamipress-discord-addon-activator.php
+++ includes/class-connect-gamipress-discord-addon-activator.php
@@ -30,7 +30,20 @@ class Connect_Gamipress_Discord_Addon_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		update_option( 'ets_gamipress_discord_send_welcome_dm', true );
+		update_option( 'ets_gamipress_discord_welcome_message', 'Hi [GP_STUDENT_NAME] ([GP_STUDENT_EMAIL]), Welcome, Your ranks [GP_RANKS] at [SITE_URL] Thanks, Kind Regards, [BLOG_NAME]' );                
+		update_option( 'ets_gamipress_discord_retry_failed_api', true );
+		update_option( 'ets_gamipress_discord_connect_button_bg_color', '#7bbc36' );
+		update_option( 'ets_gamipress_discord_disconnect_button_bg_color', '#ff0000' );
+		update_option( 'ets_gamipress_discord_loggedin_button_text', 'Connect With Discord' );
+		update_option( 'ets_gamipress_discord_non_login_button_text', 'Login With Discord' );
+		update_option( 'ets_gamipress_discord_disconnect_button_text', 'Disconnect From Discord' );
+		update_option( 'ets_gamipress_discord_kick_upon_disconnect', false ); 
+		update_option( 'ets_gamipress_discord_retry_api_count', 5 );
+		update_option( 'ets_gamipress_discord_job_queue_concurrency', 1 );
+		update_option( 'ets_gamipress_discord_job_queue_batch_size', 6 );
+		update_option( 'ets_gamipress_discord_log_api_response', false );
+		update_option( 'ets_gamipress_discord_uuid_file_name', wp_generate_uuid4() );
 	}
 
 }
